c91414236f60974d772f77a3873c6967fc9dbbfb
genglang <657578302@qq.com>
2018-11-21 00:39:34 +0800

    配置babel完成

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -4,8 +4,19 @@ module.exports={
 		app:path.join(__dirname,'../src/app.js')
 	},
 	output:{
-		filename: '[name].hash.js',
+		filename: '[name].[hash].js',
 		path:path.join(__dirname,'../dist'),
 		publicPath: '/public'
+	},
+	module: {
+		rules: [
+			{
+				test:/.(js|jsx)$/,
+				loader: 'babel-loader',
+				exclude:[
+					path.join(__dirname,'../node_module')
+				]
+			}
+		]
 	}
 }
