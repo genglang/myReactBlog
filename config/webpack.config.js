ab7a904ac70d0930aca13f7462fe5d6644afb4b0
genglang <657578302@qq.com>
2018-11-25 00:44:27 +0800

    webpack配置完毕

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,28 +1,46 @@
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
-module.exports={
-	entry:{
-		app:path.join(__dirname,'../src/app.js')
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+	entry: {
+		app: path.join(__dirname, '../src/app.js')
 	},
-	output:{
+	output: {
 		filename: '[name].[hash].js',
-		path:path.join(__dirname,'../dist'),
-		publicPath: '/public'
+		path: path.join(__dirname, '../dist'),
+		publicPath: ''
 	},
 	module: {
 		rules: [
 			{
-				test:/.(js|jsx)$/,
+				test: /.(js|jsx)$/,
 				loader: 'babel-loader',
-				exclude:[
-					path.join(__dirname,'../node_module')
+				exclude: [
+					path.join(__dirname, '../node_module')
 				]
 			}
 		]
 	},
 	plugins: [
 		new HTMLPlugin({
-			template:path.join(__dirname,'../src/index.html')
+			template: path.join(__dirname, '../src/index.html')
 		})
 	]
 }
+if(isDev){
+	config.devServer ={
+		host: '127.0.0.1',
+		port: '8888',
+		contentBase:path.join(__dirname,'../dist'),
+		// hot: true,
+		overlay:{ // 错误弹窗
+			errors:true // 只显示错误
+		},
+		publicPath: '', // 必须和output对应
+		historyApiFallback:{
+			index: './index.html' // 所有的404请求全部访问这
+		}
+	}
+}
+module.exports = config
--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default class App extends React.Component {
 	render(){
 		return(
-			<div>React is being very populate</div>
+			<div>React is being very populate!</div>
 		)
 	}
 }
