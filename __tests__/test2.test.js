ae8a9996bdc7bb5ab0fe58216bdbb3a4d871ba1d
genglang <657578302@qq.com>
2018-11-26 23:01:47 +0800

    添加单元测试demo

--- __tests__/test2.test.js
+++ __tests__/test2.test.js
@@ -1,10 +1,30 @@
-const sum = require('../src/utils/test2').default
+import {sum, forEach} from '../src/utils/test2'
 
 describe('加法', () => {
-	it('2+2', () => {
-		const a = 2
-		const b = 2
-		const result = 2 + 2
-		expect(sum(a, b)).toBe(result)
-	})
+    it('2+2', () => {
+        const a = 2
+        const b = 2
+        const result = 2 + 2
+        expect(sum(a, b)).toBe(result)
+    })
+})
+
+describe('forEach', () => {
+    it('遍历测试', () => {
+        const arr = []
+        const callBack = (...args) => {
+            arr.push(args)
+        }
+        forEach([1, 2], callBack)
+        expect(arr.length).toBe(2)
+        expect(arr[0][0]).toBe(1)
+        expect(arr[1][0]).toBe(2)
+    })
+    it('遍历测试mock',()=>{
+        const mockFn=jest.fn()
+        forEach([1,2],mockFn)
+        expect(mockFn.mock.calls.length).toBe(2)
+        expect(mockFn.mock.calls[0][0]).toBe(1)
+        expect(mockFn.mock.calls[1][0]).toBe(2)
+    })
 })
--- src/utils/test2.js
+++ src/utils/test2.js
@@ -1,3 +1,9 @@
-export default function sum(a, b) {
-	return a + b;
+export function sum(a, b) {
+    return a + b
+}
+
+export function forEach(items, cb) {
+    for (let index = 0; index < items.length; index++) {
+        cb(items[index])
+    }
 }
